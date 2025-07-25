ca3d317f590f83f6d53a801825e4b998a005ec0c
fathy61 <ahmedfathy15993@gmail.com>
2025-07-25 16:41:13 +0300

    home-slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components/layout/Navbar'
 import { Footer } from './components/layout/footer'
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import Slider from '../components/sections/Slider'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <Slider />
   )
 }
